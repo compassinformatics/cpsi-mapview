01b9e31b4ebe41e4f5eb3da757e1cc6b3e9fc7bd
Marc Jansen <jansen@terrestris.de>
2018-08-31 17:30:57 +0200

    Introduce an autoconnecting LayerTree

---
+++ app/view/LayerTree.js
@@ -0,0 +1,68 @@
+/**
+ * A Layertree which autoconnects to the map of the application.
+ */
+Ext.define('CpsiMapview.view.LayerTree', {
+    extend: 'Ext.tree.Panel',
+    xtype: 'layertree',
+    requires: [
+        'BasiGX.util.Map',
+        'GeoExt.data.store.LayersTree'
+    ],
+
+    // So that instanciation works without errors, might be changed during
+    // instanciation of the LayerTree.
+    store: {},
+    rootVisible: false,
+    viewConfig: {
+        plugins: { ptype: 'treeviewdragdrop' }
+    },
+
+    /**
+     * Costructor which either directly assigns the GeoExt.data.store.LayersTree
+     * or sets up a listener which does this as soon as the application itself
+     * fires the `mapready`-event.
+     *
+     * @param  {Object} cfg The configuration of the tree which we may change.
+     */
+    constructor: function(cfg) {
+        var me = this;
+        var mapComp = BasiGX.util.Map.getMapComponent();
+        var map = mapComp && mapComp.getMap();
+        if (map) {
+            cfg.store = me.makeLayerStore(map);
+        } else {
+            CpsiMapview.getApplication().on('mapready', me.autoConnectToMap);
+        }
+        me.callParent([cfg]);
+    },
+
+    /**
+     * Gueses the mapcomponent and assigns the appropriate layers store, if one
+     * could be guessed. This is bound as listener to the mapready event of the
+     * application.
+     *
+     * TODO this may change to use the passed map view from the mapready event.
+     */
+    autoConnectToMap: function() {
+        var me = this;
+        var mapComp = BasiGX.util.Map.getMapComponent();
+        var map = mapComp && mapComp.getMap();
+        if (map) {
+            var store = me.makeLayerStore(map);
+            me.setStore(store);
+        }
+    },
+
+    /**
+     * Given a `ol.Map` this method will return an instance of the GeoExt class
+     * GeoExt.data.store.LayersTree, which will work on the topmost layergroup.
+     *
+     * @param  {ol.Map} map The map to create the store from.
+     * @return {GeoExt.data.store.LayersTree} The created store.
+     */
+    makeLayerStore: function(map) {
+        return Ext.create('GeoExt.data.store.LayersTree', {
+            layerGroup: map.getLayerGroup()
+        });
+    }
+});
