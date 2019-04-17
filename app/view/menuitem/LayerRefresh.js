27d7a921a4dd2f4bc4e74cd2d9fff8e9a1af61e9
Christian Mayer <chris@meggsimum.de>
2019-04-17 16:15:51 +0200

    Add LayerRefresh MenuItem (ContextMenu)

---
+++ app/view/menuitem/LayerRefresh.js
@@ -0,0 +1,64 @@
+/**
+ * MenuItem to force redraw of a layer.
+ *
+ * @class CpsiMapview.view.menuitem.LayerRefresh
+ */
+Ext.define('CpsiMapview.view.menuitem.LayerRefresh', {
+    extend: 'Ext.menu.Item',
+    xtype: 'cmv_menuitem_layerrefresh',
+    requires: [],
+
+    /**
+     * The connected layer for this item.
+     *
+     * @cfg {ol.layer.Base}
+     */
+    layer: null,
+
+    /**
+     * Text shown in this MenuItem
+     * @cfg {String}
+     */
+    text: 'Refresh',
+
+
+    /**
+     * @private
+     */
+    initComponent: function () {
+        var me = this;
+        var allowRefresh = false;
+
+        if (me.layer) {
+            allowRefresh = me.layer.get('refreshLayerOption');
+        }
+
+        me.handler = me.handlerFunc;
+
+        me.callParent();
+
+        me.setHidden(!allowRefresh);
+    },
+
+    /**
+     * Executed when this menu item is clicked.
+     * Forces redraw of the connected layer.
+     */
+    handlerFunc: function () {
+        var me = this;
+        var source = me.layer.getSource();
+
+        // mostly WMS layers
+        if (source.updateParams) {
+            var params = source.getParams();
+            params.noCache = new Date().getMilliseconds();
+            source.updateParams(params);
+            source.refresh();
+        }
+
+        // mostly WFS layers
+        if (source instanceof ol.source.Vector) {
+            source.clear();
+        }
+    }
+});
