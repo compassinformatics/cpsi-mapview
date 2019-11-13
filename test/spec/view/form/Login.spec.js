d460eb343ef751dc83d2ef1bd941227b41afbde6
sethg <sethg@geographika.co.uk>
2019-11-13 16:58:42 +0100

    Add login tests

---
+++ test/spec/view/form/Login.spec.js
@@ -0,0 +1,31 @@
+describe('CpsiMapview.view.form.Login', function() {
+    describe('Basics', function() {
+        it('is defined', function() {
+            expect(CpsiMapview.view.form.Login).not.to.be(undefined);
+        });
+
+        it('can be instantiated', function() {
+            var inst = Ext.create('CpsiMapview.view.form.Login');
+            expect(inst).to.be.a(CpsiMapview.view.form.Login);
+        });
+
+        it('can call mocked service', function (done) {
+            var inst = Ext.create('CpsiMapview.view.form.Login', {
+                serviceUrl: '/resources/data/forms/login.json',
+                tokenName: 'mytoken'
+            });
+
+            var ctrlr = inst.getController();
+            ctrlr.attemptLogin();
+
+            ctrlr.on('login', function (jsonData) {
+                var token = Ext.util.Cookies.get('mytoken');
+                expect(token).to.be('bfdb15be-7ec9-4956-b2b3-5b25f77d3877');
+                expect(jsonData.roles.length).to.be(9);
+                done();
+            });
+
+        });
+
+    });
+});
