615ac553774e41d46f3b1174e9d3393bd7744bcb
sethg <sethg@geographika.co.uk>
2020-07-21 15:15:55 +0200

    Ensure grid height doesn't fill whole window when paging deactivated

--- app/controller/grid/Grid.js
+++ app/controller/grid/Grid.js
@@ -297,9 +297,12 @@ Ext.define('CpsiMapview.controller.grid.Grid', {
         var store = grid.getStore();
 
         // save the initial store parameters
-
         if (!me.originalPageSize) {
             me.originalPageSize = store.pageSize;
+        }
+
+        if (!me.originalHeight) {
+            me.originalHeight = grid.getHeight();
         }
 
         if (checked) {
@@ -309,6 +312,9 @@ Ext.define('CpsiMapview.controller.grid.Grid', {
             store.pageSize = null;
             store.currentPage = 1;
             store.startIndex = 0;
+            // avoid the grid resizing to fill up the whole screen
+            // set it to the height before paging was deactivated
+            grid.setHeight(me.originalHeight);
         }
 
         store.loadWfs();
