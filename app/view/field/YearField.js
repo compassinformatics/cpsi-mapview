fdb92bd5652af24ccc9f15c2b592a39f2172195f
Leonardo Spina <leo@babieleo.it>
2020-08-26 09:45:28 +0100

    new field type CpsiMapview.view.field.YearField (#266)
    * bug in error message display fixed
    
    * CpsiMapview.view.field.YearField added
    
    * Comments removed

---
+++ app/view/field/YearField.js
@@ -0,0 +1,15 @@
+Ext.define('CpsiMapview.view.field.YearField', {
+    extend: 'Ext.form.field.Number',
+    xtype: 'cmv_yearfield',
+    fieldStyle: 'text-align: right;',
+    allowDecimals: false,
+    minValue: 1990,
+    maxValue: 2030,
+    minLength: 4,
+    maxLength: 4,
+    enforceMaxLength: true,
+    maxLengthText: 'Must be in the format YYYY',
+    minLengthText: 'Must be in the format YYYY',
+    emptyText: 'Enter a year...',
+    width: 220
+});
