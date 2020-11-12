66b716605294593ea29a5f3fe845654eb258fc32
sethg <sethg@geographika.co.uk>
2020-11-12 16:02:43 +0100

    Set window to modal

--- app/plugin/ExpandPanel.js
+++ app/plugin/ExpandPanel.js
@@ -54,7 +54,7 @@ Ext.define('CpsiMapview.plugin.ExpandPanel', {
         me.maxedWin = Ext.create('Ext.window.Window', {
             maximized: true,
             closable: false,
-
+            modal: true,
             tools: [
                 {
                     type: 'restore',
