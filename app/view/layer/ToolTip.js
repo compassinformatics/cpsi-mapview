6c8af63875b146a85849ede57f08bc9ead790afd
sethg <sethg@geographika.co.uk>
2021-06-24 16:50:24 +0200

    Set thumbnail size

--- app/view/layer/ToolTip.js
+++ app/view/layer/ToolTip.js
@@ -140,9 +140,11 @@ Ext.define('CpsiMapview.view.layer.ToolTip', {
         Ext.each(me.toolTipConfig, function (tipConf) {
             if(tipConf.thumbnail && tipConf.property){
                 var antiCache = new Date().getTime();
-                var noThumbnail = 'this.style.display="none"';
+                var onError = 'this.style.display =\'none\'';
+                var style = 'width:120px; height:90px;';
                 var url = Ext.String.format(tipConf.thumbnail, feature.get(tipConf.property));
-                htmlPart = Ext.String.format('<br /><img src="{0}?_ac={1}" onerror="{2}" />', url, antiCache, noThumbnail);
+                htmlPart = Ext.String.format('<img src="{0}?_ac={1}" onerror="{2}" style="{3}"/>',
+                                             url, antiCache, onError, style);
             } else {
                 var key = tipConf.alias || tipConf.property;
                 var value = feature.get(tipConf.property);
