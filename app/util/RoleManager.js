363538a180714bff7e9cbe2bc4aa495087aa50aa
sethg <sethg@geographika.co.uk>
2021-06-25 15:37:15 +0200

    Add role manager class

---
+++ app/util/RoleManager.js
@@ -0,0 +1,28 @@
+﻿/**
+ * A singleton class used to check user roles
+ * stored in cookies
+ *
+ * @class CpsiMapview.util.RoleManager
+ */
+Ext.define('CpsiMapview.util.RoleManager', {
+    singleton: true,
+    /**
+     * Check if the user has the role by checking all the roles
+     * returned by the login service and saved in a cookie
+     * @param {any} role
+     */
+    checkRole: function (role) {
+
+        var hasRole = false;
+        var roles = Ext.util.Cookies.get('roles');
+
+        if (roles) {
+            // create a list of roles and check for the role name
+            if (roles.split(',').indexOf(role) >= 0)
+                hasRole = true;
+        }
+
+        return hasRole;
+    }
+
+});
