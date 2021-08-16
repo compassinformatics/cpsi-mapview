f6a81b88e09c31427f61f4ba579b60b9badb3f91
Seth G <sethg@geographika.co.uk>
2021-08-16 16:47:18 +0200

    Add form mixin tests (#470)
    * Add controller test
    
    * Add sample mixin test

---
+++ test/spec/form/ControllerMixin.spec.js
@@ -0,0 +1,49 @@
+describe('CpsiMapview.form.ControllerMixin', function () {
+
+    it('is defined', function () {
+        expect(CpsiMapview.form.ControllerMixin).not.to.be(undefined);
+    });
+
+    it('can be instantiated', function () {
+        var inst = Ext.create('CpsiMapview.form.ControllerMixin');
+        expect(inst).to.be.a(CpsiMapview.form.ControllerMixin);
+    });
+
+    describe('Functions', function () {
+
+        var editForm;
+
+        Ext.define('TestModel', {
+            extend: 'Ext.data.Model',
+            fields: ['id']
+        });
+
+        Ext.define('TestController', {
+            extend: 'Ext.app.ViewController',
+            alias: 'controller.TestController',
+            mixins: ['CpsiMapview.form.ControllerMixin']
+        });
+
+        beforeEach(function () {
+
+            var model = Ext.create('TestModel');
+
+            // create a new view with the controller and mixin
+            editForm = Ext.create('Ext.window.Window', {
+                viewModel: {
+                    currentRecord: model
+                },
+                controller: {
+                    type: 'TestController'
+                }
+            });
+
+        });
+
+        it('#onFieldChanged is defined', function () {
+            var ctrl = editForm.getController();
+            var fn = ctrl.onFieldChanged;
+            expect(fn).not.to.be(undefined);
+        });
+    });
+});
