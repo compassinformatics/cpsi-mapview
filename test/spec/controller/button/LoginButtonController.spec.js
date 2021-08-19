820935f1f16c38410c158c291055e6e3922bee5a
sethg <sethg@geographika.co.uk>
2021-08-19 15:26:14 +0200

    Add tests

--- test/spec/controller/button/LoginButtonController.spec.js
+++ test/spec/controller/button/LoginButtonController.spec.js
@@ -8,5 +8,26 @@ describe('CpsiMapview.controller.button.LoginButtonController', function() {
             var ctrl = new CpsiMapview.controller.button.LoginButtonController();
             expect(ctrl).to.not.be(undefined);
         });
+
+        it('can get login details', function () {
+            var ctrl = new CpsiMapview.controller.button.LoginButtonController();
+
+            Ext.util.Cookies.set('username', 'test');
+            Ext.util.Cookies.set('roles', 'Browser_Role1,Browser_Role2');
+
+            var text = ctrl.getLoginDetails();
+            expect(text).to.be('Username: <b>test</b><br />User Roles: <br /><br />Role1<br />Role2');
+        });
+
+        it('can call onClick which clears login cookie', function () {
+            var ctrl = new CpsiMapview.controller.button.LoginButtonController();
+
+            Ext.util.Cookies.set('username', 'test');
+            Ext.util.Cookies.set('roles', 'Browser_Role1,Browser_Role2');
+
+            var text = ctrl.onClick();
+            expect(Ext.util.Cookies.get('username')).to.be('');
+            expect(Ext.util.Cookies.get('roles')).to.be('');
+        });
     });
 });
---
+++ test/spec/view/button/LoginButton.spec.js
@@ -0,0 +1,12 @@
+describe('CpsiMapview.view.button.LoginButton', function() {
+    describe('Basics', function() {
+        it('is defined', function() {
+            expect(CpsiMapview.view.button.LoginButton).not.to.be(undefined);
+        });
+
+        it('can be instantiated', function() {
+            var inst = Ext.create('CpsiMapview.view.button.LoginButton');
+            expect(inst).to.be.a(CpsiMapview.view.button.LoginButton);
+        });
+    });
+});
