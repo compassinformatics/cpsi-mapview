7e9ceeef0abf2e45da3e6c772844c80141c0c7dc
sethg <sethg@geographika.co.uk>
2021-09-09 23:24:42 +0200

    Add tests for new functionality

--- test/spec/form/ControllerMixin.spec.js
+++ test/spec/form/ControllerMixin.spec.js
@@ -45,5 +45,98 @@ describe('CpsiMapview.form.ControllerMixin', function () {
             var fn = ctrl.onFieldChanged;
             expect(fn).not.to.be(undefined);
         });
+
+        it('#onDigitizingToolToggle flag and listeners are set', function () {
+            var ctrl = editForm.getController();
+
+            var source = new ol.source.Vector();
+            var resultLayer = new ol.layer.Vector({ source: source });
+
+            editForm.getViewModel().set('resultLayer', resultLayer)
+            expect(ctrl.toolListenerAdded).to.be(false);
+            expect(source.getListeners('featuresupdated')).to.be(undefined);
+            ctrl.onDigitizingToolToggle();
+            expect(ctrl.toolListenerAdded).to.be(true);
+            expect(source.getListeners('featuresupdated').length).to.be(1);
+            ctrl.onDigitizingToolToggle();
+            // listener count should remain the same
+            expect(source.getListeners('featuresupdated').length).to.be(1);
+        });
+
+        describe('Digitizing', function () {
+
+            var polygonLayer;
+
+            beforeEach(function () {
+                var feature = new ol.Feature({ id: 'foo' });
+                polygonLayer = new ol.layer.Vector({
+                    source: new ol.source.Vector({
+                        features: new ol.Collection([feature])
+                    })
+                });
+
+                editForm.getViewModel().set('polygonLayer', polygonLayer)
+            });
+
+            it('#onEdgesModified polygons are removed when Point tool is used', function () {
+
+                var ctrl = editForm.getController();
+
+                var evt = {
+                    modifications: {
+                        newEdgeCount: 3,
+                        toolType: 'Point'
+                    }
+                }
+
+                ctrl.onEdgesModified(evt);
+                expect(polygonLayer.getSource().getFeatures().length).to.be(0);
+            });
+
+            it('#onEdgesModified polygons are not removed when no new edges have been added', function () {
+
+                var ctrl = editForm.getController();
+
+                var evt = {
+                    modifications: {
+                        newEdgeCount: 0,
+                        toolType: 'Point'
+                    }
+                }
+
+                ctrl.onEdgesModified(evt);
+                expect(polygonLayer.getSource().getFeatures().length).to.be(1);
+            });
+
+            it('#onEdgesModified polygons are not removed when Polygon tool is used', function () {
+
+                var ctrl = editForm.getController();
+
+                var evt = {
+                    modifications: {
+                        newEdgeCount: 3,
+                        toolType: 'Polygon'
+                    }
+                }
+
+                ctrl.onEdgesModified(evt);
+                expect(polygonLayer.getSource().getFeatures().length).to.be(1);
+            });
+
+            it('#onEdgesModified polygons are not removed when Circle tool is used', function () {
+
+                var ctrl = editForm.getController();
+
+                var evt = {
+                    modifications: {
+                        newEdgeCount: 3,
+                        toolType: 'Circle'
+                    }
+                }
+
+                ctrl.onEdgesModified(evt);
+                expect(polygonLayer.getSource().getFeatures().length).to.be(1);
+            });
+        });
     });
 });
