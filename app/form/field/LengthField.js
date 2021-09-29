b29ab2bbd0c1b8b6bd9d1c32ec7c921df30b3381
Anna Rieger <rieger@terrestris.de>
2021-09-29 10:15:10 +0200

    introduce length field (#506)

---
+++ app/form/field/LengthField.js
@@ -0,0 +1,9 @@
+
+Ext.define('CpsiMapview.form.field.LengthField',
+    {
+        extend: 'CpsiMapview.form.field.NumericField',
+        xtype: 'cmv_lengthfield',
+        symbol: 'm',
+        decimalPrecision: 2
+    }
+);
