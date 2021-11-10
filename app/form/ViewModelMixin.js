d45a0485160f260503391c76d15d9fdea857c2f4
sethg <sethg@geographika.co.uk>
2021-11-10 14:44:19 +0100

    Add valid method for clarity

--- app/form/ViewModelMixin.js
+++ app/form/ViewModelMixin.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * A mixin for an editing windows providing formulas that can be bound
  * to the window model
  *
@@ -104,9 +104,18 @@ Ext.define('CpsiMapview.form.ViewModelMixin', {
                 }
             },
 
+            /**
+            * This is overwritten in most of the ViewModels that use this mixin
+            * @param {any} get
+            */
+            valid: function (get) {
+                return true;
+            },
+
             canSave: {
                 bind: {
                     currentRecord: '{currentRecord}',
+                    isLocked: '{currentRecord.isLocked}',
                     isValid: '{valid}'
                 },
                 get: function (data) {
