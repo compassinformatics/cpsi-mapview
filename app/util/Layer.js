b1eadbc350734e995b7c8a92208bc86198db9560
Seth G <sethg@geographika.co.uk>
2022-04-13 14:16:04 +0200

    Refresh layer node when using the Refresh plugin (#554)
    * Refresh layer node when using the Refresh plugin to also update legends
    
    * Fix filter glyph bug
    
    * Lint fix
    
    * Return if no layer tree found in app

--- app/util/Layer.js
+++ app/util/Layer.js
@@ -65,6 +65,11 @@ Ext.define('CpsiMapview.util.Layer', {
             // only refresh for other layers and sources (to not loose data)
             source.refresh();
         }
+
+        // also refresh the layer node, which updates any legend which may be broken
+        // if a user's access token expires
+        var layerUtil = CpsiMapview.util.Layer;
+        layerUtil.updateLayerNodeUI(layer);
     },
 
     /**
@@ -77,6 +82,12 @@ Ext.define('CpsiMapview.util.Layer', {
         // we will only ever have one layer tree for an application
 
         var treePanel = Ext.ComponentQuery.query('cmv_layertree:first')[0];
+
+        if (!treePanel) {
+            Ext.log.warn('No cmv_layertree found in the application (updateLayerNodeUI)');
+            return;
+        }
+
         var node = treePanel.getNodeForLayer(layer);
 
         if (!node) {
@@ -105,7 +116,9 @@ Ext.define('CpsiMapview.util.Layer', {
             node.set('glyph', 'f0b0@FontAwesome');
             node.addCls('cpsi-tree-node-filtered');
         } else {
-            node.set('glyph', null);
+            // revert to the original glyph if set on the layer
+            var glyph = layer.get('_origTreeConf') ? layer.get('_origTreeConf').glyph : null;
+            node.set('glyph', glyph);
             node.removeCls('cpsi-tree-node-filtered');
         }
 
