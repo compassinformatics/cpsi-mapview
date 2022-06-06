01c921aaf4f2d93687624a5d12d1e27bc07176ab
geographika <sethg@geographika.co.uk>
2022-06-06 19:19:29 +0100

    Add new draw tool

---
+++ test/spec/controller/button/DrawButtonController.spec.js
@@ -0,0 +1,13 @@
+describe('CpsiMapview.controller.button.DrawButtonController', function () {
+    describe('Basics', function () {
+        it('is defined', function () {
+            expect(CpsiMapview.controller.button.DrawButtonController).not.to.be(undefined);
+        });
+
+        it('can be created', function () {
+            var ctrl = new CpsiMapview.controller.button.DrawButtonController();
+            expect(ctrl).to.not.be(undefined);
+        });
+
+    });
+});
