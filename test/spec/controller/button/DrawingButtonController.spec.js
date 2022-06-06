77b5743984a3fcda82e8244696e333c911e50eb5
geographika <sethg@geographika.co.uk>
2022-06-06 19:26:32 +0100

    Update class name

---
+++ test/spec/controller/button/DrawingButtonController.spec.js
@@ -0,0 +1,13 @@
+describe('CpsiMapview.controller.button.DrawingButtonController', function () {
+    describe('Basics', function () {
+        it('is defined', function () {
+            expect(CpsiMapview.controller.button.DrawingButtonController).not.to.be(undefined);
+        });
+
+        it('can be created', function () {
+            var ctrl = new CpsiMapview.controller.button.DrawingButtonController();
+            expect(ctrl).to.not.be(undefined);
+        });
+
+    });
+});
