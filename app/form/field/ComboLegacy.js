f0013476e19006abb6ab9d4fd69056595179df3d
Seth G <sethg@geographika.co.uk>
2022-06-15 14:21:26 +0200

    Add customised combobox classes (#565)

---
+++ app/form/field/ComboLegacy.js
@@ -0,0 +1,52 @@
+/**
+ * A combobox that allows any legacy lookup values to be displayed
+ * for existing records, but not selected for new records
+ *
+ * @class CpsiMapview.form.field.ComboLegacy
+ */
+Ext.define('CpsiMapview.form.field.ComboLegacy', {
+    extend: 'CpsiMapview.form.field.Combo',
+    xtype: 'cmv_combolegacy',
+
+    // the following property can be set to add a label to the combobox entries
+    legacyPostfix: ' - Legacy',
+
+    /**
+    * Attempt to prevent legacy values from being selected with the keyboard
+    * However using the select event stops the combobox being populated
+    * when a form is loaded
+    **/
+    //listeners: {
+    //    select: function (combo, record) {
+    //        if (record.get('isLegacy')) {
+    //            combo.clearValue();
+    //        }
+    //    }
+    //},
+
+    /**
+     * See https://docs.sencha.com/extjs/6.7.0/classic/Ext.XTemplate.html for docs on templates
+     * And the Sencha forums at https://www.sencha.com/forum/showthread.php?308501-Combo-box-TPL-if-condition
+     * and associated fiddle https://fiddle.sencha.com/#fiddle/14ud&view/editor
+     **/
+    initComponent: function () {
+        var me = this;
+        var itemTemplate = Ext.String.format(
+            '<tpl for=".">' +
+            '<tpl if="isLegacy === true">' +
+            '<li role="option" class="' + Ext.baseCSSPrefix + 'boundlist-item ' +
+            Ext.baseCSSPrefix + 'item-disabled ' +
+            Ext.baseCSSPrefix + 'boundlist-item-legacy"> ' +
+            '{{0}}' + me.legacyPostfix +
+            '</li>' +
+            '<tpl else>' +
+            '<li role="option" class="' + Ext.baseCSSPrefix + 'boundlist-item">' +
+            '{{0}}' +
+            '</li>' +
+            '</tpl>' +
+            '</tpl>', me.displayField);
+
+        me.tpl = itemTemplate;
+        me.callParent(arguments);
+    }
+});
