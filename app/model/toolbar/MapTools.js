76e7811530838fe0aa83d24578d4439d9c29c7ad
sethg <sethg@geographika.co.uk>
2022-07-08 13:04:50 +0200

    Add a ViewModel to the tools to store a loggedIn property

---
+++ app/model/toolbar/MapTools.js
@@ -0,0 +1,9 @@
+Ext.define('CpsiMapview.model.toolbar.MapToolsModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.cmv_maptools',
+
+    data: {
+        loggedIn: false // used to keep track of if a user is logged in or not
+    }
+});
