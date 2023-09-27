88a0f3f92142b309f0eed330e1e707bd861be469
sethg <sethg@geographika.co.uk>
2023-09-27 10:29:10 +0200

    Add tests for styling refactoring

--- test/spec/util/Legend.spec.js
+++ test/spec/util/Legend.spec.js
@@ -11,59 +11,6 @@ describe('CpsiMapview.util.Legend', function () {
         });
     });
 
-    describe('#getWmsStyleFromSldFile', function () {
-        it('derives a style from file name', function () {
-            var fileName = 'LAYERS_STYLES.xml';
-            var styleName = cmp.getWmsStyleFromSldFile(fileName);
-            expect(styleName).to.equal('STYLES');
-        });
-
-        it('replaces underscores in style name with spaces', function () {
-            var fileName = 'LAYERS_STYLES_WITH_SPACES.xml';
-            var styleName = cmp.getWmsStyleFromSldFile(fileName);
-            expect(styleName).to.equal('STYLES WITH SPACES');
-        });
-    });
-
-    describe('#getSldFilefromWmsStyle', function() {
-        it('derives the sld file name from style and layer', function () {
-            var layerName = 'FOO';
-            var styleName = 'BAR';
-
-            var fileName = cmp.getSldFileFromWmsStyle(styleName, layerName);
-            expect(fileName).to.equal('FOO_BAR.xml');
-        });
-
-        it('replaces spaces in style with underscores', function () {
-            var layerName = 'FOO';
-            var styleName = 'BAR BAZ';
-
-            var fileName = cmp.getSldFileFromWmsStyle(styleName, layerName);
-            expect(fileName).to.equal('FOO_BAR_BAZ.xml');
-        });
-    });
-
-    describe('#hasLabels', function () {
-
-        it('returns true, if all styles have labels', function () {
-            var styles = [{
-                label: 'foo'
-            }, {
-                label: 'bar'
-            }];
-            var hasLabels = cmp.hasLabels(styles);
-            expect(hasLabels).to.be(true);
-        });
-
-        it('returns false, if at least one style has no label', function () {
-            var styles = [{
-                label: 'foo'
-            }, {}];
-            var hasLabels = cmp.hasLabels(styles);
-            expect(hasLabels).to.be(false);
-        });
-    });
-
     describe('#getUniqueLayersParam', function () {
         it('removes duplicate layers from list', function () {
             var layers = ['foo', 'foo'];
