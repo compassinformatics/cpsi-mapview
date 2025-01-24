7b9220ce3b5e01c497dbaeaf90c0b76d335580f5
Seth G <sethg@geographika.co.uk>
2025-01-24 14:52:30 +0100

    Add override for bug in ExtJS 7.8 (#696)

---
+++ app/overrides/Ext.grid.header.Container.js
@@ -0,0 +1,21 @@
+﻿Ext.override(Ext.grid.header.Container, {
+    getHeaderMenu: function () {
+        var menu = this.getMenu(),
+            item;
+
+        if (menu) {
+            item = menu.child('#columnItem');
+            // no check to see if item is null here
+            // TypeError: Cannot read properties of null (reading 'menu')
+            // so move within guard block
+            // item.menu.hideOnScroll = false;
+
+            if (item) {
+                item.menu.hideOnScroll = false;
+                return item.menu;
+            }
+        }
+
+        return null;
+    },
+});
